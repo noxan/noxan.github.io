26d9e8ab1b8b8ccd6422f73f4aca4b145701fab6
noxan <noxan@byteweaver.net>
2013-04-29 11:36:40 +0200

    add script to center project modal

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -3,3 +3,19 @@
     $('#myCarousel').carousel();
   });
 }(window.jQuery);
+
+$(function() {
+  function centerModal() {
+    $('#projects-modal').css({
+      'top': function () {
+        return ($(window).height()-$(this).height())/2;
+      }
+    });
+  }
+
+  centerModal();
+
+  $(window).resize(function() {
+    centerModal();
+  });
+});
