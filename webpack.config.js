831537a02899557b631a564cbfc76c65e4d26afd
Richard <richard@devisio.net>
2015-06-07 22:52:31 +0200

    add image loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader" }
+      { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader" },
+      { test: /\.(jpe?g|png|gif|svg)$/i, loaders: [ 'file?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false' ] }
     ]
   },
   devServer: {
