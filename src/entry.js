dc677bb0e5526a1ddc74c081981047c3845310d8
Richard <richard@devisio.net>
2015-06-07 19:40:24 +0200

    setup css, style and stylus loaders with webpack

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,3 @@
 console.log("hello world.");
+
+var css = require("./main.styl");
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,10 @@ module.exports = {
   output: {
     path: "./dist/",
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader" }
+    ]
   }
 };
