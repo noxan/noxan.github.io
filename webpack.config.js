0c88769f114eb31888566464b31945e8690375fa
Richard <richard@devisio.net>
2015-06-08 11:25:00 +0200

    setup jade-loader
    thanks to https://github.com/markmarijnissen/angular-webpack-seed/blob/master/webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   module: {
     loaders: [
+      { test: /(?!\.html)\.jade$/, loader: "jade-loader" },
       { test: /index\.html\.jade$/, loader: "file-loader?name=[path][name]&context=./src!jade-html-loader" },
       { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader" },
       { test: /\.(jpe?g|png|gif|svg)$/i, loaders: [ 'file?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false' ] }
