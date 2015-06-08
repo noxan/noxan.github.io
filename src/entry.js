3ea4260b4b02149b3e6751f4aad51d7f8b96f7ea
Richard <richard@devisio.net>
2015-06-08 11:22:13 +0200

    start to setup angular-ui-router

--- src/entry.js
+++ src/entry.js
@@ -3,5 +3,15 @@ require("./index.html.jade");
 var css = require("./main.styl");
 
 var angular = require("angular");
+var router = require("angular-ui-router");
 
-angular.module("noxan", []);
+angular.module("noxan", ["ui.router"]);
+
+angular.module("noxan").config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+  $stateProvider.state("home", {
+    url: "/",
+    templateUrl: "partials/home.html"
+  });
+
+  $urlRouterProvider.otherwise("/");
+}]);
