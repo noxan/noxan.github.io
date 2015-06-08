3238df9ad2d7bf722b45ae049840c3f3b7e4b105
Richard <richard@devisio.net>
2015-06-08 11:34:03 +0200

    add new home partial and state with link to demo

--- src/entry.js
+++ src/entry.js
@@ -8,7 +8,10 @@ var router = require("angular-ui-router");
 angular.module("noxan", ["ui.router"]);
 
 angular.module("noxan").config(["$stateProvider", "$urlRouterProvider", "$locationProvider", function ($stateProvider, $urlRouterProvider, $locationProvider) {
-  $stateProvider.state("demo", {
+  $stateProvider.state("home", {
+    url: "/",
+    template: require("./partials/home.jade")
+  }).state("demo", {
     url: "/demo",
     template: require("./partials/demo.jade")
   });
