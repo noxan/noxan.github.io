01ce5d1542b5abc50b7a019ba29561a3ab48b238
Richard <richard@devisio.net>
2015-06-09 02:43:05 +0200

    split labs in experiments and projects

--- src/labs/index.js
+++ src/labs/index.js
@@ -3,5 +3,6 @@ angular.module("noxan.labs", []);
 angular.module("noxan.labs").controller("LabsController", [function () {
   var ctrl = this;
 
-  ctrl.experiments = require("../data/labs.json");
+  ctrl.experiments = require("../data/labs.json").experiments;
+  ctrl.projects = require("../data/labs.json").projects;
 }]);
