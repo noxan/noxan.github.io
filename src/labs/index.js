aaf5e321049a9d03ecb74f175082bd30712adb69
Richard <richard@devisio.net>
2015-06-09 13:43:04 +0200

    move labs data json to its folder

--- src/labs/index.js
+++ src/labs/index.js
@@ -3,6 +3,6 @@ angular.module("noxan.labs", []);
 angular.module("noxan.labs").controller("LabsController", [function () {
   var ctrl = this;
 
-  ctrl.experiments = require("../data/labs.json").experiments;
-  ctrl.projects = require("../data/labs.json").projects;
+  ctrl.experiments = require("./labs.data.json").experiments;
+  ctrl.projects = require("./labs.data.json").projects;
 }]);
