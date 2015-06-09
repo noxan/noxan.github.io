e893d68af5c6ddb2636ffe44ea67200a4e17d54f
Richard <richard@devisio.net>
2015-06-09 14:48:45 +0200

    hide additional technologies and show them with a button, close #2

--- src/portfolio/technologies.controller.js
+++ src/portfolio/technologies.controller.js
@@ -1,7 +1,22 @@
 function TechnologiesController(Technologies) {
   var vm = this;
 
-  vm.technologies = Technologies.all();
+  vm.technologies = Technologies.all().map(function (tech, index) {
+    tech.show = index < 10;
+    return tech;
+  });
+
+  vm.hasHidden = function () {
+    return vm.technologies.filter(function (tech) {
+      return !tech.show;
+    }).length > 0;
+  };
+
+  vm.showAll = function () {
+    vm.technologies.forEach(function (tech) {
+      tech.show = true;
+    });
+  };
 }
 TechnologiesController.$inject = ["Technologies"];
 
