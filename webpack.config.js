3749de749cbb850f4a9a5314233bc4a9a2ae424f
Richard <richard@devisio.net>
2015-06-28 01:20:50 +0200

    add autoprefixer-loader to fix animation, hover effects and centered content in safari

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
       { test: /\.json$/, loader: "json-loader" },
       { test: /(?!\.html)\.jade$/, loader: "jade-loader" },
       { test: /index\.html\.jade$/, loader: "file-loader?name=[path][name]&context=./src!jade-html-loader" },
-      { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader" },
+      { test: /\.styl$/, loader: "style-loader!css-loader!autoprefixer-loader!stylus-loader" },
       { test: /\.(jpe?g|png|gif|svg)$/i, loaders: [ 'file?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false' ] }
     ],
     preLoaders: [{
