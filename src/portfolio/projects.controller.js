7dde6d42434394ae09a3fcbdf703600eec9b88a6
Richard <richard@devisio.net>
2015-06-27 12:52:50 +0200

    open ngdialog with new detail template

--- src/portfolio/projects.controller.js
+++ src/portfolio/projects.controller.js
@@ -1,11 +1,18 @@
-function ProjectsController(Projects) {
+function ProjectsController(Projects, ngDialog) {
   var vm = this;
 
   vm.projects = Projects.all();
 
   vm.showDetails = function (project) {
+    ngDialog.open({
+      template: require("./projects.detail.tmpl.jade")(),
+      plain: true,
+      controller: ["$scope", function ($scope) {
+        $scope.project = project;
+      }]
+    });
   };
 }
-ProjectsController.$inject = ["Projects"];
+ProjectsController.$inject = ["Projects", "ngDialog"];
 
 angular.module("noxan.portfolio.projects").controller("ProjectsController", ProjectsController);
